97c0907a04353755901d7d7691af06db151d1e7d
Tom Porter <thomas.porter@acm.org>
2011-11-08 19:28:45 -0500

    Removed unused code

--- starbuzz.rb
+++ starbuzz.rb
@@ -7,18 +7,9 @@ Dir["#{File.dirname(__FILE__)}/models/*.rb"].each { |model| require model }
 # automatically create the Drink table
 Drink.auto_migrate! unless Drink.storage_exists?
 
-def gen_entries
-  entries = []
-  entries << {name:'House Blend',price:'$1.49',description:'A smooth, mild blend of coffees from Mexico, Bolivia and Guatemala.'}
-  entries << {name:'Mocha Cafe Latte',price:'$2.35',description:'Espresso, steamed milk and chocolate syrup.'}
-  entries << {name:'Cappucino',price:'$1.89',description:'A mixture of espresso, steamed milk and foam.'}
-  entries << {name:'Chai Tea',price:'$1.85',description:'A spicy drink made with black tea, spices, milk and honey.'}
-  entries
-end
 get '/' do
   #@foo = 'for Tom Porter'
   @foo = ''
-  #@drinks = Drink.get(:order => [ :id ], :limit => 20)
   @drinks = Drink.all
   haml :index
 end
