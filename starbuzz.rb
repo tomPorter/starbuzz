211636785b51d5339ca791e6892b02dc46a259a8
Tom Porter <thomas.porter@acm.org>
2011-11-08 20:14:07 -0500

    Migrate all models, not just drink

--- starbuzz.rb
+++ starbuzz.rb
@@ -3,9 +3,12 @@ require 'sinatra'
 require 'data_mapper'
 require 'haml'
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/mydatabase.db")
-Dir["#{File.dirname(__FILE__)}/models/*.rb"].each { |model| require model }
-# automatically create the Drink table
-Drink.auto_migrate! unless Drink.storage_exists?
+Dir["#{File.dirname(__FILE__)}/models/*.rb"].each do |model| 
+  require model 
+  if model.include? 'drink' 
+    Drink.auto_migrate! unless Drink.storage_exists?
+  end
+end
 
 get '/' do
   #@foo = 'for Tom Porter'
