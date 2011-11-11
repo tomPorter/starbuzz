03a45f50fc59e0a5c43abbe37499096c81371eaf
Tom Porter <tom.x.porter@gmail.com>
2011-11-11 16:16:30 -0500

    Added test for hidden text using css selector matcher

--- features/index.feature
+++ features/index.feature
@@ -3,3 +3,4 @@ Feature: Home page
   Scenario: Reading the "hello world" message on the home page
     When I go to the home page
     Then I should see "Starbuzz Coffee"
+    And There should be hidden text "Beverage#list_static"
--- features/step_definitions/sinatra_steps.rb
+++ features/step_definitions/sinatra_steps.rb
@@ -5,3 +5,6 @@ end
 Then /^I should see "([^\"]*)"$/ do |text|
   response_body.should =~ Regexp.new(Regexp.escape(text))
 end
+Then /^There should be hidden text "([^\"]*)"$/ do |text|
+  response_body.should have_selector('.hideme',:content => text)
+end
