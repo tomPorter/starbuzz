b7bfb6b7dfadcbdc9ec963753c15403650167833
Tom Porter <tom.x.porter@gmail.com>
2011-11-11 17:01:35 -0500

    Figure out how to do nested css selectors

--- features/step_definitions/sinatra_steps.rb
+++ features/step_definitions/sinatra_steps.rb
@@ -6,5 +6,10 @@ Then /^I should see "([^\"]*)"$/ do |text|
   response_body.should =~ Regexp.new(Regexp.escape(text))
 end
 Then /^There should be hidden text "([^\"]*)"$/ do |text|
-  response_body.should have_selector('.hideme',:content => text)
+  #response_body.should have_selector('.hideme',:content => text)
+  response.body.should have_selector("#content") do |content|
+    content.should have_selector('.hideme') do |hidden|
+      hidden.should have_selector('h3',:content => text)
+    end
+  end
 end
